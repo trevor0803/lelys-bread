5df23c8d7358caf41803128a26b1667f1aa9ae16
trevor0803 <trevor@mat-digital.com>
2026-07-29 16:35:28 -0400

    Switch ordering from SMS to Hotplate

--- site-config.js
+++ site-config.js
@@ -1,8 +1,6 @@
 window.LELYS_CONFIG = {
-  // Fictional placeholder number. Replace with the bakery's real number before launch.
-  orderPhone: "15615550147",
   hotplateSlug: "lelysbread",
-  hotplateUrl: "https://www.hotplate.com/lelysbread?referralMember=861c015e-dfcc-44cd-971f-c0a830c55ad8&referralType=reminder",
-  locationLabel: "Wellington, Florida",
-  defaultMessage: "Hi! I'd like to request an order from Lely's Bread. What is available this week?"
+  hotplateUrl: "https://www.hotplate.com/lelysbread",
+  hotplateOrderUrl: "https://www.hotplate.com/lelysbread?event=current",
+  locationLabel: "Wellington, Florida"
 };
