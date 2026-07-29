65703f6bec0c61dde4f6b80d342c3f79ae26a144
trevor0803 <trevor@mat-digital.com>
2026-07-29 16:25:10 -0400

    Add bakery configuration with placeholder phone

---
+++ site-config.js
@@ -0,0 +1,8 @@
+window.LELYS_CONFIG = {
+  // Fictional placeholder number. Replace with the bakery's real number before launch.
+  orderPhone: "15615550147",
+  hotplateSlug: "lelysbread",
+  hotplateUrl: "https://www.hotplate.com/lelysbread?referralMember=861c015e-dfcc-44cd-971f-c0a830c55ad8&referralType=reminder",
+  locationLabel: "Wellington, Florida",
+  defaultMessage: "Hi! I'd like to request an order from Lely's Bread. What is available this week?"
+};
